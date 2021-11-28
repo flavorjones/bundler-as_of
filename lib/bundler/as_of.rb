0d30a16525827def84214d5b02afab8af3335e75
Mike Dalessio <mike.dalessio@gmail.com>
2021-11-28 09:49:29 -0500

    bundler gem
    bundler gem bundler-as_of \
      --coc --mit --test=minitest \
      --ci=github --linter=rubocop --no-exe

---
+++ lib/bundler/as_of.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "as_of/version"
+
+module Bundler
+  module AsOf
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bundler/as_of/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bundler
+  module AsOf
+    VERSION = "0.1.0"
+  end
+end
