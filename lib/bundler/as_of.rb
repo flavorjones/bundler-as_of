9d38c7a573717482ea05c5d5749210bb8201fcd1
Mike Dalessio <mike.dalessio@gmail.com>
2021-11-28 10:56:26 -0500

    find a matching version and modify the dependency requirements

--- lib/bundler/as_of.rb
+++ lib/bundler/as_of.rb
@@ -1,27 +1,38 @@
 # frozen_string_literal: true
 
-require_relative "as_of/version"
 require "date"
+require "open-uri"
+require "json"
 
 module Bundler
   module AsOf
     BUNDLE_AS_OF = "BUNDLE_AS_OF"
+    LEAVE_ALONE = ["bundler"]
 
     class InstallModifier
       def modify_dependencies(dependencies)
-        return unless as_of_date
-        warn "NOTE: bundler-as_of: bundling dependencies as of #{as_of_date} ..."
+        if as_of_date.nil?
+          warn("NOTE: bundler-as_of is installed but #{BUNDLE_AS_OF} is not set")
+          return
+        end
 
-        dependencies.each do |dep|
-          pp [dep.name, dep.requirement, dep.requirements_list]
+        warn("NOTE: bundler-as_of: bundling dependencies as of #{as_of_date} ...")
+        dependencies.each do |dependency|
+          next if LEAVE_ALONE.include?(dependency.name)
+          orig_req = dependency.requirements_list
+          release = VersionFinder.new(dependency, as_of_date).resolve
+          if release
+            warn("NOTE: bundler-as_of: resolving #{dependency.name} #{orig_req} to #{release.version} released on #{release.date}")
+            set_dependency_requirement(dependency, release.version)
+          else
+            warn("NOTE: bundler-as_of: WARNING: could not resolve #{dependency.name} to a version matching #{dependency.requirements_list} from #{as_of_date}")
+          end
         end
       end
 
       def as_of_date
-        if as_of_env.nil?
-          warn "NOTE: bundler-as_of is installed but #{BUNDLE_AS_OF} is not set"
-          return nil
-        end
+        return nil if as_of_env.nil?
+
         @as_of_date ||= begin
           Date.parse(as_of_env)
         rescue Date::Error
@@ -29,10 +40,56 @@ module Bundler
         end
       end
 
-      private
-
       def as_of_env
         ENV[BUNDLE_AS_OF]
+      end
+
+      def set_dependency_requirement(dependency, *requirements)
+        dependency.instance_variable_set(:@requirement, Gem::Requirement.create(requirements))
+      end
+    end
+
+    class VersionFinder
+      def initialize(dependency, as_of_date)
+        @dependency = dependency
+        @as_of_date = as_of_date
+      end
+
+      def resolve
+        releases.each do |release|
+          return release if @dependency.requirement.satisfied_by?(release.version)
+        end
+        nil
+      end
+
+      def releases
+        gem_releases
+          .select { |r| r.date <= @as_of_date }
+          .sort_by { |r| [r.date, r.version] }
+          .reverse
+      end
+
+      def gem_releases
+        @gem_releases ||=
+          JSON.parse(::URI.parse(gem_url).open.read)
+            .map { |r| ReleaseWrapper.new(@dependency.name, r) }
+      end
+
+      def gem_url
+        @gem_url ||= "https://rubygems.org/api/v1/versions/#{@dependency.name}.json"
+      end
+    end
+
+    class ReleaseWrapper
+      attr_reader :name, :version, :date
+      def initialize(name, release_json)
+        @name = name
+        @version = Gem::Version.new(release_json["number"])
+        @date = Date.parse(release_json["created_at"])
+      end
+
+      def to_s
+        [name, version.to_s, date.to_s].to_s
       end
     end
   end
@@ -41,3 +98,5 @@ end
 Bundler::Plugin.add_hook("before-install-all") do |dependencies|
   Bundler::AsOf::InstallModifier.new.modify_dependencies(dependencies)
 end
+
+require_relative "as_of/version"
