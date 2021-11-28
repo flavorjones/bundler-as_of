43669e32bc9c1bee24afad71932d52baa18aef7e
Mike Dalessio <mike.dalessio@gmail.com>
2021-11-28 09:58:43 -0500

    playing around with bundler plugin behavior

--- lib/bundler/as_of.rb
+++ lib/bundler/as_of.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
+puts "MIKE: #{__FILE__}:#{__LINE__}:#{__method__}"
 require_relative "as_of/version"
 
 module Bundler
   module AsOf
-    class Error < StandardError; end
-    # Your code goes here...
+    class InstallModifier
+      def modify_dependencies(dependencies)
+        puts "MIKE: #{__FILE__}:#{__LINE__}:#{__method__}"
+        pp dependencies
+      end
+    end
   end
+end
+
+Bundler::Plugin.add_hook("before-install-all") do |dependencies|
+  Bundler::AsOf::InstallModifier.new.modify_dependencies(dependencies)
 end
---
+++ plugins.rb
@@ -0,0 +1,1 @@
+require "bundler/as_of"
